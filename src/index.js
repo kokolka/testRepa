ca6e1216c1de0a477ea4668dfb4b754ac0f5b32b
Kirll Balahoncev <kirill-i_98@mail.ru>
2022-06-29 21:13:52 +0700

    change for deploy

--- src/index.js
+++ src/index.js
@@ -3,20 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  BrowserRouter
-} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './redux/redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
